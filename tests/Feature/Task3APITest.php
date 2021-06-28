1c606e31400d6c01a881c536008fc9b54862893b
Rakib Hassan <rakib@piit.local>
2021-06-28 18:09:55 +0600

    Task-3 Taste case is completed

--- tests/Feature/Task3APITest.php
+++ tests/Feature/Task3APITest.php
@@ -2,21 +2,47 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Order;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class Task3APITest extends TestCase
 {
+    use RefreshDatabase,WithFaker;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_can_get_pending_order_list()
     {
-        $response = $this->get('/');
-
+        $orders=Order::factory()->create();
+        $response = $this->json('GET', route('order.index'));
         $response->assertStatus(200);
+
+        $response->assertJsonStructure(
+            [
+                'pendingOrders' =>
+                    [
+                        '*' => [
+                            'user_id',
+                            'orderID',
+                            'mobile',
+                            'address',
+                            'status'
+                       ]
+                    ]
+            ]
+        );
+    }
+    public function test_can_update_stock()
+    {
+        $order=Order::factory()->create();
+        $formData=[
+            "status" => 'delivered',
+        ];
+        $this->withoutExceptionHandling();
+        $this->json('PUT',route('order.update',$order->id),$formData)->assertStatus(200);
     }
 }

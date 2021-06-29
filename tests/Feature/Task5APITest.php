b9f4765d12547bd9ec248debe3e9a5b8b282234d
Rakib Hassan <rakib@piit.local>
2021-06-29 17:32:24 +0600

    TestCase completed for Task5API

--- tests/Feature/Task5APITest.php
+++ tests/Feature/Task5APITest.php
@@ -2,27 +2,24 @@
 
 namespace Tests\Feature;
 
-use App\Models\OfferOrder;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Stock;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class Task4APITest extends TestCase
+class Task5APITest extends TestCase
 {
     use RefreshDatabase,WithFaker;
-
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_can_order_with_offer()
+    public function test_can_view_related_products_after_completing_the_order()
     {
         $user=User::factory()->create();
         $stock=Stock::factory()->create();
-        $offerOrder=OfferOrder::factory()->create();
         $formData=[
             "user_id"=> $user->id,
             "orderID"=> rand(111111,9999999).date('ymdhmi'),
@@ -54,44 +51,14 @@ class Task4APITest extends TestCase
                             'deliveryCost'
                         ]
                     ]
-                ]
-            ]
-        );
-    }
-    public function test_can_order_with_preOrderCode()
-    {
-        $user=User::factory()->create();
-        $stock=Stock::factory()->create();
-        $formData=[
-            "user_id"=> $user->id,
-            "orderID"=> rand(111111,9999999).date('ymdhmi'),
-            "mobile"=> $this->faker->phoneNumber,
-            "address"=> $this->faker->randomElement(['chitagong', 'feni', 'dhaka']),
-            "stock_id"=> [$stock->id,$stock->id],
-            "quantity"=> [$this->faker->randomElement([5, 3, 7, 10]),$this->faker->randomElement([5, 3, 7, 10])],
-            "preOrderCode"=>""
-        ];
-        $this->withoutExceptionHandling();
-        $response=$this->json('POST',route('order.store'),$formData)->assertStatus(200);
-        
-        $response->assertJsonStructure(
-            [
-                'invoice' 
-                    =>[
-                    'orderInfo'=>[
-                        'user_id',
-                        'mobile',
-                        'address',
-                        'orderID'
-                    ],
-                    'orderProducts'=>[
-                        '*'=>[
-                            'productName',
-                            'order_id',
-                            'stock_id',
-                            'totalProductPrice',
-                            'deliveryCost'
-                        ]
+                ],
+                'relatedProducts'=>[
+                    '*'=>[
+                        'product_id',
+                        'weight',
+                        'tasteType',
+                        'pricePerBox',
+                        'boxQuantity'
                     ]
                 ]
             ]
